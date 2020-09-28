a694a89ac968f0a844065e5ce41bf3fee6ec2e81
Henrique Laki <17856028+henriquelaki@users.noreply.github.com>
2020-09-28 19:31:35 -0300

    Initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+npm init -y
+npm install express --save
+npm install mongoose --save
+npm install dotenv --save
+npm install colors --save
+npm install helmet --save
+npm install cors --save
+npm install -g nodemon
+npm install nodemon --save-dev
+npm install morgan --save-dev
